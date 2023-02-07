abcfcdbccc83f5a0405141933a6d9ae3e8560572
Fuyukiri <ns34gp@hotmail.com>
2023-02-07 00:23:07 -0800

    minor fix to pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -10,25 +10,22 @@ main()
 
     if (fork() == 0)
     {
-        char byte;
-        read(p[0], &byte, 1);
+        
+        read(p[0], "0", 1);
         close(p[0]);
         printf("%d: received ping\n", getpid());
-        write(p[1], &byte, 1);
+        write(p[1], "0", 1);
         close(p[1]);
         exit(0);
     }
     else
     {
-        char byte;
-        write(p[1], &byte, 1);
+        write(p[1], "0", 1);
         close(p[1]);
-        wait(0);
-        read(p[0], &byte, 1);
+        read(p[0], "0", 1);
         close(p[0]);
         printf("%d: received pong\n", getpid());
         exit(0);
     }
-
     return 0;
 }
